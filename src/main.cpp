bb5bc8499f7c4abc0a5c285d9deeadc2f2a92db9
RobotProgrammerMan <snoozy283@gmail.com>
2024-02-15 16:42:30 -0500

    Added 6 Motor Drive

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,11 @@ controller Controller1 = controller(); // Controller
 motor TopLeft = motor(PORT1, ratio18_1, true); // Top Left Drive Motor
 motor BottomLeft = motor(PORT9, ratio6_1, true); // Bottom Left Drive Motor
 motor TopRight = motor(PORT2, ratio18_1, false); // Top Right Drive Motor
-motor BottomRight = motor(PORT10, ratio6_1, false); // Bottom Right Drive Motor
-motor SpinnySpin = motor(PORT5, ratio6_1, true); // Flywheel Motor
-digital_out wingPistonA(Brain.ThreeWirePort.A); // Wing
+motor BottomRight = motor(PORT8, ratio6_1, false); // Bottom Right Drive Motor
+motor ExtraLeft = motor(PORT16, ratio18_1, true);
+motor ExtraRight = motor(PORT17, ratio18_1, false);
+motor SpinnySpin = motor(PORT4, ratio6_1, false); // Flywheel Motor
+digital_out wingPistonA(Brain.ThreeWirePort.C); // Wing
 digital_out wingPistonB(Brain.ThreeWirePort.B); // Wing
 
 // Variables
@@ -46,6 +48,8 @@ void Stop() {
   TopLeft.stop();
   BottomRight.stop();
   BottomLeft.stop();
+  ExtraLeft.stop();
+  ExtraRight.stop();
 }
 
 void Right() {
@@ -53,6 +57,8 @@ void Right() {
   TopLeft.spin(forward);
   BottomRight.spin(reverse);
   BottomLeft.spin(forward);
+  ExtraLeft.spin(forward);
+  ExtraRight.spin(reverse);
 }
 
 void Left() {
@@ -60,6 +66,8 @@ void Left() {
   TopLeft.spin(reverse);
   BottomRight.spin(forward);
   BottomLeft.spin(reverse);
+  ExtraLeft.spin(reverse);
+  ExtraRight.spin(forward);
 }
 
 void Forward() {
@@ -67,6 +75,8 @@ void Forward() {
   TopLeft.spin(forward);
   BottomRight.spin(forward);
   BottomLeft.spin(forward);
+  ExtraLeft.spin(forward);
+  ExtraRight.spin(forward);
 }
 
 void Reverse() {
@@ -74,20 +84,26 @@ void Reverse() {
   TopLeft.spin(reverse);
   BottomRight.spin(reverse);
   BottomLeft.spin(reverse);
+  ExtraLeft.spin(reverse);
+  ExtraRight.spin(reverse);
 }
 
 void MoveBiatch(double moveNumber) {
   TopRight.spinFor(moveNumber, rev, false);
   TopLeft.spinFor(moveNumber, rev, false);
   BottomRight.spinFor(moveNumber, rev, false);
-  TopLeft.spinFor(moveNumber, rev, true);
+  TopLeft.spinFor(moveNumber, rev, false);
+  ExtraLeft.spinFor(moveNumber, rev, false);
+  ExtraRight.spinFor(moveNumber, rev, true);
 }
 
 void TurnBiatch(double turnNumber) {
   TopRight.spinFor(-turnNumber, rev, false);
   BottomRight.spinFor(-turnNumber, rev, false);
   TopLeft.spinFor(turnNumber, rev, false);
-  BottomRight.spinFor(turnNumber, rev, true);
+  BottomRight.spinFor(turnNumber, rev, false);
+  ExtraLeft.spinFor(turnNumber, rev, false);
+  ExtraRight.spinFor(-turnNumber, rev, true);
 }
 
 
@@ -97,11 +113,15 @@ void pre_auton(void) {
   TopRight.setBrake(brake);
   BottomLeft.setBrake(brake);
   BottomRight.setBrake(brake);
+  ExtraRight.setBrake(brake);
+  ExtraLeft.setBrake(brake);
 
   TopLeft.setVelocity(50, percent);
   TopRight.setVelocity(50, percent);
   BottomLeft.setVelocity(50, percent);
   BottomRight.setVelocity(50, percent);
+  ExtraRight.setVelocity(50, percent);
+  ExtraLeft.setVelocity(50, percent);
   SpinnySpin.setVelocity(100, percent);
 
   wingPistonA.set(1);
@@ -120,6 +140,8 @@ void autonomous(void) {
   TopRight.setVelocity(25, percent);
   BottomLeft.setVelocity(25, percent);
   BottomRight.setVelocity(25, percent);
+  ExtraLeft.setVelocity(25, percent);
+  ExtraRight.setVelocity(25, percent);
   Reverse();
   wait(1.5, sec);
   Stop();
@@ -130,11 +152,34 @@ void autonomous(void) {
   TopRight.setVelocity(100, percent);
   BottomLeft.setVelocity(100, percent);
   BottomRight.setVelocity(100, percent);
+  ExtraLeft.setVelocity(100, percent);
+  ExtraRight.setVelocity(100, percent);
   Right();
-  wait(.35, sec);
+  wait(.65, sec);
   Stop();
   Forward();
-  wait(2.5, sec);
+  wait(2.6, sec);
+  Stop();
+  wait(.1, sec);
+  Left();
+  wait(.7, sec);
+  Stop();
+  Forward();
+  wait(1, sec);
+  Stop();
+  Right();
+  wait(.8, sec);
+  Stop();
+  wait(.1, sec);
+  wingPistonA.set(0);
+  wingPistonB.set(0);
+  Forward();
+  wait(1.5, sec);
+  Stop();
+  wingPistonA.set(1);
+  wingPistonB.set(1);
+  Reverse();
+  wait(1, sec);
   Stop();
 }
 
